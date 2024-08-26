28e3818ddb75c47ccf87e12be32ddd1510ae60f3
Fortune-Ndlovu <fndlovu@redhat.com>
2024-08-26 16:40:21 +0100

    Assign IIB

--- .rhdh/scripts/install-registry-proxy-images.sh
+++ .rhdh/scripts/install-registry-proxy-images.sh
@@ -34,7 +34,7 @@ skopeo copy --all "docker://$IIB_IMAGE" "oci:$WORKDIR/iib"
 
 # Unpack the OCI image
 cd "$WORKDIR"
-echo "Listing contents of blobs/sha256/ for debugging..."
+echo "Listing contents of blobs/sha256/ for debugging..."gity
 ls -R "$WORKDIR/iib/blobs/sha256/"
 
 # Attempt to inspect index.json to locate manifests
