8da5b2f09b744ecae4815db1af9eb5e39068792f
Armel Soro <asoro@redhat.com>
2024-11-29 18:21:42 +0100

    Add dedicated function that installs the CatalogSource and Subscription on a standard Kubernetes cluster
    Just like with OCP clusters with hosted control planes
    (where we cannot use ICSP/IDMS resources),
    we are doing the following here:
    1. Deploy an authenticated registry (there may not be any internal registry available, unlike OCP)
    2. Expose this registry via port-forwarding and using a NodePort (to be able to pull it from within the cluster)
    3. Replacing all references to the internal RH registry with quay.io in the bundles, repack and push the bundles to the registry above
    4. Regenerate the IIB and build it in the cluster (using Kaniko, because the script might be called from CI where Podman cannot run)
    5. Create the CatalogSource using this new IIB ref

--- .rhdh/scripts/install-rhdh-catalog-source.sh
+++ .rhdh/scripts/install-rhdh-catalog-source.sh
@@ -46,6 +46,28 @@ Examples:
 
 if [[ "$#" -lt 1 ]]; then usage; exit 0; fi
 
+function is_openshift() {
+  oc get routes.route.openshift.io &> /dev/null || kubectl get routes.route.openshift.io &> /dev/null
+}
+
+IS_OPENSHIFT=$(is_openshift && echo 'true' || echo 'false')
+
+# Wrapper function to call kubectl or oc
+function invoke_cluster_cli() {
+  local command=$1
+  shift
+
+  if [[ "${IS_OPENSHIFT}" = "true" ]]; then
+    if command -v oc &> /dev/null; then
+      oc $command "$@"
+    else
+      kubectl $command "$@"
+    fi
+  else
+    kubectl $command "$@"
+  fi
+}
+
 # minimum requirements
 if [[ ! $(command -v oc) ]]; then
   errorf "Please install oc 4.10+ from an RPM or https://mirror.openshift.com/pub/openshift-v4/clients/ocp/"
@@ -223,18 +245,77 @@ spec:
   printf "%s" "${IIB_IMAGE}"
 }
 
-function install_hosted_control_plane_cluster() {
+function render_iib() {
+  mkdir -p "${TMPDIR}/rhdh/rhdh"
+  echo "[DEBUG] Rendering IIB $UPSTREAM_IIB as a local file..."
+  opm render "$UPSTREAM_IIB" --output=yaml > "${TMPDIR}/rhdh/rhdh/render.yaml"
+  if [ ! -s "${TMPDIR}/rhdh/rhdh/render.yaml" ]; then
+    errorf "
+[ERROR] 'opm render $UPSTREAM_IIB' returned an empty output, which likely means that this IIB Image does not contain any operators in it.
+Please reach out to the RHDH Productization team.
+"
+    exit 1
+  fi
+}
+
+function update_refs_in_iib_bundles() {
+  local internal_registry_url="$1"
+  local my_registry="$2"
+  # 2. Render the IIB locally, modify any references to the internal registries with their mirrors on Quay
+    # and push the updates to the internal cluster registry
+    for bundleImg in $(cat "${TMPDIR}/rhdh/rhdh/render.yaml" | grep -E '^image: .*operator-bundle' | awk '{print $2}' | uniq); do
+      originalBundleImg="$bundleImg"
+      digest="${originalBundleImg##*@sha256:}"
+      bundleImg="${bundleImg/registry.stage.redhat.io/quay.io}"
+      bundleImg="${bundleImg/registry.redhat.io/quay.io}"
+      bundleImg="${bundleImg/registry-proxy.engineering.redhat.com\/rh-osbs\/rhdh-/quay.io\/rhdh\/}"
+      echo "[DEBUG] $originalBundleImg => $bundleImg"
+      if skopeo inspect "docker://$bundleImg" &> /dev/null; then
+        newBundleImage="${my_registry}/rhdh/rhdh-operator-bundle:${digest}"
+        newBundleImageAsInt="${internal_registry_url}/rhdh/rhdh-operator-bundle:${digest}"
+        mkdir -p "bundles/$digest"
+
+        echo "[DEBUG] Copying and unpacking image $bundleImg locally..."
+        skopeo copy "docker://$bundleImg" "oci:./bundles/${digest}/src:latest"
+        umoci unpack --image "./bundles/${digest}/src:latest" "./bundles/${digest}/unpacked" --rootless
+
+        # Replace the occurrences in the .csv.yaml or .clusterserviceversion.yaml files
+        echo "[DEBUG] Replacing refs to internal registry in bundle image $bundleImg..."
+        for folder in manifests metadata; do
+          for file in "./bundles/${digest}/unpacked/rootfs/${folder}"/*; do
+            if [ -f "$file" ]; then
+              echo "[DEBUG] replacing refs to internal registries in file '${file}'"
+              sed -i 's#registry.redhat.io/rhdh#quay.io/rhdh#g' "$file"
+              sed -i 's#registry.stage.redhat.io/rhdh#quay.io/rhdh#g' "$file"
+              sed -i 's#registry-proxy.engineering.redhat.com/rh-osbs/rhdh-#quay.io/rhdh/#g' "$file"
+            fi
+          done
+        done
+
+        # repack the image with the changes
+        echo "[DEBUG] Repacking image ./bundles/${digest}/src => ./bundles/${digest}/unpacked..."
+        umoci repack --image "./bundles/${digest}/src:latest" "./bundles/${digest}/unpacked"
+
+        # Push the bundle to the internal cluster registry
+        echo "[DEBUG] Pushing updated image: ./bundles/${digest}/src => ${newBundleImage}..."
+        skopeo copy --dest-tls-verify=false "oci:./bundles/${digest}/src:latest" "docker://${newBundleImage}"
+
+        sed -i "s#${originalBundleImg}#${newBundleImageAsInt}#g" "${TMPDIR}/rhdh/rhdh/render.yaml"
+      fi
+    done
+
+    # 3. Regenerate the IIB image with the local changes to the render.yaml file and build and push it from within the cluster
+    echo "[DEBUG] Regenerating IIB Dockerfile with updated refs..."
+    opm generate dockerfile rhdh/rhdh
+}
+
+function ocp_install_hosted_control_plane_cluster() {
   # Clusters with an hosted control plane do not propagate ImageContentSourcePolicy/ImageDigestMirrorSet resources
   # to the underlying nodes, causing an issue mirroring internal images effectively.
   # This function works around this by locally modifying the bundles (replacing all refs to the internal registries
   # with their mirrors on quay.io), rebuilding and pushing the images to the internal cluster registry.
-  mkdir -p "${TMPDIR}/rhdh/rhdh" >&2
-  echo "[DEBUG] Rendering IIB $UPSTREAM_IIB as a local file..." >&2
-  opm render "$UPSTREAM_IIB" --output=yaml > "${TMPDIR}/rhdh/rhdh/render.yaml"
-  if [ ! -s "${TMPDIR}/rhdh/rhdh/render.yaml" ]; then
-    errorf "[ERROR] 'opm render $UPSTREAM_IIB' returned an empty output, which likely means that this IIB Image does not contain any operators in it. Please reach out to the RHDH Productization team." >&2
-    exit 1
-  fi
+
+  render_iib >&2
 
   # 1. Expose the internal cluster registry if not done already
   echo "[DEBUG] Exposing cluster registry..." >&2
@@ -269,52 +350,8 @@ function install_hosted_control_plane_cluster() {
   oc policy add-role-to-user system:image-puller system:serviceaccount:openshift-marketplace:default -n openshift-marketplace >&2 || true
   oc policy add-role-to-user system:image-puller system:serviceaccount:rhdh-operator:default -n rhdh-operator >&2 || true
 
-  # 2. Render the IIB locally, modify any references to the internal registries with their mirrors on Quay
-  # and push the updates to the internal cluster registry
-  for bundleImg in $(cat "${TMPDIR}/rhdh/rhdh/render.yaml" | grep -E '^image: .*operator-bundle' | awk '{print $2}' | uniq); do
-    originalBundleImg="$bundleImg"
-    digest="${originalBundleImg##*@sha256:}"
-    bundleImg="${bundleImg/registry.stage.redhat.io/quay.io}"
-    bundleImg="${bundleImg/registry.redhat.io/quay.io}"
-    bundleImg="${bundleImg/registry-proxy.engineering.redhat.com\/rh-osbs\/rhdh-/quay.io\/rhdh\/}"
-    echo "[DEBUG] $originalBundleImg => $bundleImg" >&2
-    if skopeo inspect "docker://$bundleImg" &> /dev/null; then
-      newBundleImage="${my_registry}/rhdh/rhdh-operator-bundle:${digest}"
-      newBundleImageAsInt="${internal_registry_url}/rhdh/rhdh-operator-bundle:${digest}"
-      mkdir -p "bundles/$digest" >&2
-
-      echo "[DEBUG] Copying and unpacking image $bundleImg locally..." >&2
-      skopeo copy "docker://$bundleImg" "oci:./bundles/${digest}/src:latest" >&2
-      umoci unpack --image "./bundles/${digest}/src:latest" "./bundles/${digest}/unpacked" --rootless >&2
-
-      # Replace the occurrences in the .csv.yaml or .clusterserviceversion.yaml files
-      echo "[DEBUG] Replacing refs to internal registry in bundle image $bundleImg..." >&2
-      for folder in manifests metadata; do
-        for file in "./bundles/${digest}/unpacked/rootfs/${folder}"/*; do
-          if [ -f "$file" ]; then
-            echo "[DEBUG] replacing refs to internal registries in file '${file}'" >&2
-            sed -i 's#registry.redhat.io/rhdh#quay.io/rhdh#g' "$file" >&2
-            sed -i 's#registry.stage.redhat.io/rhdh#quay.io/rhdh#g' "$file" >&2
-            sed -i 's#registry-proxy.engineering.redhat.com/rh-osbs/rhdh-#quay.io/rhdh/#g' "$file" >&2
-          fi
-        done
-      done
-
-      # repack the image with the changes
-      echo "[DEBUG] Repacking image ./bundles/${digest}/src => ./bundles/${digest}/unpacked..." >&2
-      umoci repack --image "./bundles/${digest}/src:latest" "./bundles/${digest}/unpacked" >&2
-
-      # Push the bundle to the internal cluster registry
-      echo "[DEBUG] Pushing updated image: ./bundles/${digest}/src => ${newBundleImage}..." >&2
-      skopeo copy --dest-tls-verify=false "oci:./bundles/${digest}/src:latest" "docker://${newBundleImage}" >&2
-
-      sed -i "s#${originalBundleImg}#${newBundleImageAsInt}#g" "${TMPDIR}/rhdh/rhdh/render.yaml" >&2
-    fi
-  done
-
   # 3. Regenerate the IIB image with the local changes to the render.yaml file and build and push it from within the cluster
-  echo "[DEBUG] Regenerating IIB Dockerfile with updated refs..." >&2
-  opm generate dockerfile rhdh/rhdh >&2
+  update_refs_in_iib_bundles "$internal_registry_url" "$my_registry" >&2
 
   echo "[DEBUG] Submitting in-cluster build request for the updated IIB..." >&2
   if ! oc -n rhdh get buildconfig.build.openshift.io/iib >& /dev/null; then
@@ -326,6 +363,236 @@ function install_hosted_control_plane_cluster() {
   oc tag rhdh/iib:latest "${imageStreamWithTag}" >&2
 
   local result="${internal_registry_url}/${imageStreamWithTag}"
+  echo "[DEBUG] IIB built and pushed to internal cluster registry: $result..." >&2
+  printf "%s" "${result}"
+}
+
+function k8s_install() {
+  local namespace="rhdh-operator"
+  local image="registry:2"
+  local registry_name="local-registry"
+  local username="registryuser"
+  local password=$(echo "$RANDOM" | base64 | head -c 20)
+
+  render_iib >&2
+
+  if ! invoke_cluster_cli get namespace "${namespace}" &> /dev/null; then
+    invoke_cluster_cli create namespace "${namespace}" >&2
+  fi
+
+  # We cannot use ICSP/IDMS resources here => deploy a registry where the updated IIBs will be pushed to
+  if invoke_cluster_cli -n "${namespace}" get secret "${registry_name}-auth-creds" &> /dev/null; then
+    username=$(invoke_cluster_cli -n "${namespace}" get secret "${registry_name}-auth-creds" -o json | jq -r '.data.username' | base64 -d)
+    password=$(invoke_cluster_cli -n "${namespace}" get secret "${registry_name}-auth-creds" -o json | jq -r '.data.password' | base64 -d)
+  else
+    echo "  generating auth secret for mirror registry. FYI, those creds will be stored in a secret named 'airgap-registry-auth-creds' in ${namespace} ..." >&2
+    cat <<EOF | invoke_cluster_cli apply -f - >&2
+apiVersion: v1
+kind: Secret
+type: Opaque
+metadata:
+  name: "${registry_name}-auth-creds"
+  namespace: "${namespace}"
+stringData:
+  username: "${username}"
+  password: "${password}"
+EOF
+  fi
+
+  registry_htpasswd=$(htpasswd -Bbn "${username}" "${password}")
+  cat <<EOF | invoke_cluster_cli apply -f - >&2
+apiVersion: v1
+kind: Secret
+type: Opaque
+metadata:
+  name: "${registry_name}-auth"
+  namespace: "${namespace}"
+stringData:
+  htpasswd: "${registry_htpasswd}"
+EOF
+
+  echo "[info]  creating the registry Deployment: deployment/${registry_name} ..." >&2
+  cat <<EOF | invoke_cluster_cli apply -f - >&2
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: "${registry_name}"
+  namespace: "${namespace}"
+  labels:
+    app: "${registry_name}"
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: "${registry_name}"
+  template:
+    metadata:
+      labels:
+        app: "${registry_name}"
+    spec:
+      containers:
+        - image: "${image}"
+          name: "${registry_name}"
+          imagePullPolicy: IfNotPresent
+          env:
+            - name: REGISTRY_AUTH
+              value: "htpasswd"
+            - name: REGISTRY_AUTH_HTPASSWD_REALM
+              value: "RHDH Private Registry"
+            - name: REGISTRY_AUTH_HTPASSWD_PATH
+              value: "/auth/htpasswd"
+            - name: REGISTRY_STORAGE_DELETE_ENABLED
+              value: "true"
+          ports:
+            - containerPort: 5000
+          volumeMounts:
+            - name: auth-vol
+              mountPath: "/auth"
+              readOnly: true
+      # -----------------------------------------------------------------------
+      volumes:
+        - name: auth-vol
+          secret:
+            secretName: "${registry_name}-auth"
+EOF
+
+  echo "[info]  creating the registry Service: service/${registry_name} ..." >&2
+  # NOTE: We need a NodePort here, for the kubelet to be able to pull insecurely from localhost.
+  # We cannot use the internal Service DNS name (ending with .svc.cluster.local) because the Kubelet is responsible for
+  # pulling the images and does not seem able to resolve such internal DNS names.
+  cat <<EOF | invoke_cluster_cli apply -f - >&2
+apiVersion: v1
+kind: Service
+metadata:
+  name: "${registry_name}"
+  namespace: "${namespace}"
+  labels:
+    app: "${registry_name}"
+spec:
+  type: NodePort
+  ports:
+    - port: 5000
+      protocol: TCP
+      targetPort: 5000
+  selector:
+    app: "${registry_name}"
+EOF
+  echo "Waiting for service $registry_name in namespace $namespace to become ready..." >&2
+  sleep 7
+  local registrySvcNodePort=$(invoke_cluster_cli -n "${namespace}" get service "$registry_name" -o jsonpath='{.spec.ports[0].nodePort}')
+  if [[ -z "$registrySvcNodePort" ]]; then
+    errorf "NodePort not allocated for service/$registry_name or Service not found"
+    exit 1
+  fi
+
+  echo "Waiting for deployment $registry_name in namespace $namespace to become ready..." >&2
+  if ! invoke_cluster_cli rollout status deployment/$registry_name -n $namespace --timeout=5m &>/dev/null; then
+    echo "Timed out waiting for deployment $registry_name to be ready." >&2
+    exit 1
+  fi
+
+  local registry_port_fwd_out="${TMPDIR}/k8s.registry_port_fwd.out.txt"
+  invoke_cluster_cli port-forward "service/$registry_name" -n "$namespace" :5000 &> "${registry_port_fwd_out}" &
+  local port_fwd_pid=$!
+  echo "Port-forwarding process: $port_fwd_pid" &>2
+  sleep 7
+  # Check if the port-forward is running
+  if ! kill -0 $port_fwd_pid &> /dev/null; then
+      echo "Port-forwarding to the cluster registry failed to start. Logs:" >&2
+      cat "${registry_port_fwd_out}"
+      exit 1
+  fi
+  trap "kill $port_fwd_pid || true" EXIT
+
+  local portFwdLocalPort=$(grep -oP '127\.0\.0\.1:\K[0-9]+' "${registry_port_fwd_out}")
+  if [[ -z "$portFwdLocalPort" ]]; then
+      echo "Failed to determine the local port. Logs:" >&2
+      cat "${registry_port_fwd_out}"
+      exit 1
+  fi
+  echo "[DEBUG] Port-forwarding from localhost:${portFwdLocalPort} to the cluster registry..." >&2
+
+  local kaniko_internal_registry_url="${registry_name}.${namespace}.svc.cluster.local:5000"
+  local internal_registry_url="localhost:${registrySvcNodePort}"
+
+  skopeo login -u "${username}" -p "${password}" --tls-verify=false "localhost:$portFwdLocalPort" >&2
+  invoke_cluster_cli -n "${namespace}" create secret docker-registry kaniko-registry-secret \
+      --docker-server=${kaniko_internal_registry_url} \
+      --docker-username="${username}" \
+      --docker-password="${password}" \
+      --docker-email="admin@internal-registry-ext.kaniko.example.com" >&2
+  invoke_cluster_cli -n olm create secret docker-registry internal-reg-ext-auth-for-rhdh \
+      --docker-server=${internal_registry_url} \
+      --docker-username="${username}" \
+      --docker-password="${password}" \
+      --docker-email="admin@internal-registry-ext.example.com" >&2
+  invoke_cluster_cli -n olm create secret docker-registry internal-reg-auth-for-rhdh \
+    --docker-server=${internal_registry_url} \
+      --docker-username="${username}" \
+      --docker-password="${password}" \
+    --docker-email="admin@internal-registry.example.com" >&2
+
+  # 3. Regenerate the IIB image with the local changes to the render.yaml file and build and push it from within the cluster
+  update_refs_in_iib_bundles "$internal_registry_url" "localhost:$portFwdLocalPort" >&2
+
+  local imageStreamWithTag="rhdh/iib:${IIB_TAG}"
+  local result="${internal_registry_url}/${imageStreamWithTag}"
+  local kanikoResult="${kaniko_internal_registry_url}/${imageStreamWithTag}"
+
+  # 4. Rebuild the IIB image in the cluster using Kaniko
+  echo "[DEBUG] Rebuilding the IIB Image using Kaniko in the cluster..." >&2
+  local timestamp=$(date +%s)
+  local kanikoJobName="kaniko-build-${timestamp}"
+  cat <<EOF | invoke_cluster_cli apply -f - >&2
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: "${kanikoJobName}"
+  namespace: "${namespace}"
+spec:
+  backoffLimit: 0
+  ttlSecondsAfterFinished: 3600
+  template:
+    spec:
+      containers:
+      - name: kaniko
+        image: gcr.io/kaniko-project/executor:debug
+        command: ["/bin/sh", "-c"]
+        args:
+        - |
+            while [ ! -f /workspace/context.tar.gz ]; do echo 'Waiting for the build context archive...'; sleep 2; done
+            /kaniko/executor --context=tar:///workspace/context.tar.gz --dockerfile=rhdh.Dockerfile --destination=$kanikoResult
+        volumeMounts:
+        - name: build-context
+          mountPath: /workspace
+        - name: registry-secret
+          mountPath: /kaniko/.docker/
+          readOnly: true
+      restartPolicy: Never
+      volumes:
+      - name: build-context
+        emptyDir: {}
+      - name: registry-secret
+        secret:
+          secretName: kaniko-registry-secret
+          items:
+          - key: .dockerconfigjson
+            path: config.json
+EOF
+
+  local kanikoPod=$(invoke_cluster_cli -n "${namespace}" get pods --selector=job-name="${kanikoJobName}" -o jsonpath='{.items[0].metadata.name}' || exit 1)
+  echo "[DEBUG] Waiting for Kaniko pod to be ready..." >&2
+  invoke_cluster_cli -n "${namespace}" wait --for=condition=Ready "pod/$kanikoPod" --timeout=60s >&2
+  invoke_cluster_cli -n "${namespace}" logs -f "${kanikoPod}" >&2 &
+  kanikoLogsPid=$!
+  trap "kill $kanikoLogsPid &>/dev/null || true" EXIT
+
+  local localContext=context.tar.gz
+  tar -czf "${localContext}" -C rhdh . >&2
+  invoke_cluster_cli -n "${namespace}" cp "${localContext}" "${kanikoPod}:/workspace/${localContext}" >&2 || exit 1
+
+  invoke_cluster_cli -n "${namespace}" wait --for=condition=complete "job/${kanikoJobName}" --timeout=300s >&2 || exit 1
+
   echo "[DEBUG] IIB built and pushed to internal cluster registry: $result..." >&2
   printf "%s" "${result}"
 }
