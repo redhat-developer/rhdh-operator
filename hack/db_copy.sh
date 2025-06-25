d6b86f4086ab66e71a96820e2169b3de98c103fd
Leanne Ahern <lahern@redhat.com>
2025-06-25 17:51:23 +0100

    Adding double quotes to all arrays in db_copy script. Reason: Error SC2068
    Signed-off-by: Leanne Ahern <lahern@redhat.com>

--- hack/db_copy.sh
+++ hack/db_copy.sh
@@ -10,10 +10,10 @@ from_user=postgres
 
 allDB=("backstage_plugin_app" "backstage_plugin_auth" "backstage_plugin_catalog" "backstage_plugin_permission" "backstage_plugin_scaffolder" "backstage_plugin_search")
 
-for db in ${!allDB[@]};
+for db in "${!allDB[@]};"
 do
   db=${allDB[$db]}
-  echo Copying database: $db
+  echo Copying database: "$db"
   PGPASSWORD=$TO_PSW psql -h $to_host -p $to_port -U $to_user -c "create database $db;"
-  pg_dump -h $from_host -p $from_port -U $from_user -d $db | PGPASSWORD=$TO_PSW psql -h $to_host -p $to_port -U $to_user -d $db
+  pg_dump -h $from_host -p $from_port -U $from_user -d "$db" | PGPASSWORD=$TO_PSW psql -h $to_host -p $to_port -U $to_user -d "$db"
 done
