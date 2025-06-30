eeefb32a027ab03c9ea57ce9648fa250f425797c
gazarenkov <gazarenkov@gmail.com>
2025-06-30 17:40:24 +0300

    plugin-infra.sh

---
+++ config/profile/rhdh/plugin-infra/plugin-infra.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#
+# Plugin Infrastructure Setup Script for RHDH with Orchestrator
+#
+
+action="${1:-apply}" # Default action is 'apply'
+
+kubectl $action -f - <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: openshift-serverless
+---
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: serverless-operator-group
+  namespace: openshift-serverless
+spec:
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: serverless-operator
+  namespace: openshift-serverless
+spec:
+  channel: stable
+  installPlanApproval: Automatic
+  name: serverless-operator
+  source: redhat-operators
+  sourceNamespace: openshift-marketplace
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: knative-serving
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: knative-eventing
+EOF
+
+if [ "$action" == "apply" ]; then
+  echo "Waiting for CRDs to be established..."
+  kubectl wait --for=condition=Established crd --all --timeout=60s
+fi
+
+kubectl $action -f - <<EOF
+apiVersion: operator.knative.dev/v1beta1
+kind: KnativeEventing
+metadata:
+  name: knative-eventing
+  namespace: knative-eventing
+spec:
+  Registry: {}
+---
+apiVersion: operator.knative.dev/v1beta1
+kind: KnativeServing
+metadata:
+  name: knative-serving
+  namespace: knative-serving
+spec:
+  controller-custom-certs:
+    name: ""
+    type: ""
+  registry: {}
+EOF
+
+kubectl $action -f - <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: openshift-serverless-logic
+---
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: openshift-serverless-logic
+  namespace: openshift-serverless-logic
+spec:
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: logic-operator-rhel8
+  namespace: openshift-serverless-logic
+spec:
+  channel: alpha  #  channel of an operator package to subscribe to
+  installPlanApproval: Automatic #  whether the update should be installed automatically
+  name: logic-operator-rhel8  #  name of the operator package
+  source: redhat-operators  #  name of the catalog source
+  sourceNamespace: openshift-marketplace
+  startingCSV: logic-operator-rhel8.v1.35.0  # The initial version of the operator
