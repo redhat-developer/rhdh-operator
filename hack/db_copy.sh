666d602dba028f0e73fbaa33603059cbf87a0625
Leanne Ahern <127859485+OpinionatedHeron@users.noreply.github.com>
2025-07-15 13:24:52 +0100

    chore: Checking ShellCheck errors and applying relevant fixes (#1305)
    * Checking shellCheck fix - for db_copyfile
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Adding double quotes to all arrays in db_copy script. Reason: Error SC2068
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Fixing SC2145 errors in prepare-restricted-environment script
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Fixing SC2206 errors -Quote to prevent word splitting/globbing - in prepare-restricted-environment script
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Fixing comments to ignore certain warnings. Adjusting code to fix warning - SC2178 and SC2128.
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Updating db_copy.sh file with necessary changes. Quoting all variables.
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Running script and making changes to ensure it runs to completion
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Changing lines to ensure script runs correctly without errors
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Updating db_copy based on PR feedback
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Slight changes based on PR feedback
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Making changes based on suggestions
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Fixing missed errors and small mistake
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    
    * Update .rhdh/scripts/prepare-restricted-environment.sh
    
    ---------
    
    Signed-off-by: Leanne Ahern <lahern@redhat.com>
    Co-authored-by: Armel Soro <armel@rm3l.org>

--- hack/db_copy.sh
+++ hack/db_copy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-to_host=<db-service-host>
+set -euo pipefail
+
+: "${TO_PSW:?TO_PSW environment variable not set}"
+
+to_host="<db-service-host>"
 to_port=5432
 to_user=postgres
 
@@ -10,10 +14,9 @@ from_user=postgres
 
 allDB=("backstage_plugin_app" "backstage_plugin_auth" "backstage_plugin_catalog" "backstage_plugin_permission" "backstage_plugin_scaffolder" "backstage_plugin_search")
 
-for db in ${!allDB[@]};
-do
-  db=${allDB[$db]}
-  echo Copying database: $db
-  PGPASSWORD=$TO_PSW psql -h $to_host -p $to_port -U $to_user -c "create database $db;"
-  pg_dump -h $from_host -p $from_port -U $from_user -d $db | PGPASSWORD=$TO_PSW psql -h $to_host -p $to_port -U $to_user -d $db
+for db in "${allDB[@]}"; do
+  db=${allDB["$db"]}
+  echo Copying database: "$db"
+  PGPASSWORD="$TO_PSW" psql -h "$to_host" -p "$to_port" -U "$to_user" -c "create database \"$db\";"
+  pg_dump -h "$from_host" -p "$from_port" -U "$from_user" -d "$db" | PGPASSWORD="$TO_PSW" psql -h "$to_host" -p "$to_port" -U "$to_user" -d "$db"
 done
