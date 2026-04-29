3ef8b9f16626535e0f4d16e1ae2858817c43e4d5
gazarenkov <gazarenkov@gmail.com>
2026-04-29 18:46:12 +0300

    do not delete disabled resources

--- integration_tests/db_test.go
+++ integration_tests/db_test.go
@@ -9,11 +9,7 @@ import (
 
 	"k8s.io/utils/ptr"
 
-	corev1 "k8s.io/api/core/v1"
-
 	appsv1 "k8s.io/api/apps/v1"
-
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/redhat-developer/rhdh-operator/api"
 
@@ -39,52 +35,52 @@ var _ = When("create backstage with CR configured", func() {
 		deleteNamespace(ctx, ns)
 	})
 
-	It("creates default Backstage and then update CR to not to use local DB", func() {
-		backstageName := createAndReconcileBackstage(ctx, ns, api.BackstageSpec{}, "")
-
-		Eventually(func(g Gomega) {
-			By("creating Deployment with database.enableLocalDb=true by default")
-
-			err := k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-%s", backstageName)}, &appsv1.StatefulSet{})
-			g.Expect(err).To(Not(HaveOccurred()))
-
-			err = k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-%s", backstageName)}, &corev1.Service{})
-			g.Expect(err).To(Not(HaveOccurred()))
-
-			err = k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-secret-%s", backstageName)}, &corev1.Secret{})
-			g.Expect(err).To(Not(HaveOccurred()))
-
-		}, time.Minute, time.Second).Should(Succeed())
-
-		By("updating Backstage")
-		update := &api.Backstage{}
-		err := k8sClient.Get(ctx, types.NamespacedName{Name: backstageName, Namespace: ns}, update)
-		Expect(err).To(Not(HaveOccurred()))
-		update.Spec.Database = &api.Database{}
-		update.Spec.Database.EnableLocalDb = ptr.To(false)
-		err = k8sClient.Update(ctx, update)
-		Expect(err).To(Not(HaveOccurred()))
-		_, err = NewTestBackstageReconciler(ns).ReconcileAny(ctx, reconcile.Request{
-			NamespacedName: types.NamespacedName{Name: backstageName, Namespace: ns},
-		})
-		Expect(err).To(Not(HaveOccurred()))
-
-		Eventually(func(g Gomega) {
-			By("deleting Local Db StatefulSet, Service and Secret")
-			err = k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-%s", backstageName)}, &appsv1.StatefulSet{})
-			g.Expect(err).To(HaveOccurred())
-			g.Expect(errors.IsNotFound(err))
-
-			err = k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-%s", backstageName)}, &corev1.Service{})
-			g.Expect(err).To(HaveOccurred())
-			g.Expect(errors.IsNotFound(err))
-
-			err = k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-secret-%s", backstageName)}, &corev1.Secret{})
-			g.Expect(err).To(HaveOccurred())
-			g.Expect(errors.IsNotFound(err))
-		}, time.Minute, time.Second).Should(Succeed())
-
-	})
+	//It("creates default Backstage and then update CR to not to use local DB", func() {
+	//	backstageName := createAndReconcileBackstage(ctx, ns, api.BackstageSpec{}, "")
+	//
+	//	Eventually(func(g Gomega) {
+	//		By("creating Deployment with database.enableLocalDb=true by default")
+	//
+	//		err := k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-%s", backstageName)}, &appsv1.StatefulSet{})
+	//		g.Expect(err).To(Not(HaveOccurred()))
+	//
+	//		err = k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-%s", backstageName)}, &corev1.Service{})
+	//		g.Expect(err).To(Not(HaveOccurred()))
+	//
+	//		err = k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-secret-%s", backstageName)}, &corev1.Secret{})
+	//		g.Expect(err).To(Not(HaveOccurred()))
+	//
+	//	}, time.Minute, time.Second).Should(Succeed())
+	//
+	//	By("updating Backstage")
+	//	update := &api.Backstage{}
+	//	err := k8sClient.Get(ctx, types.NamespacedName{Name: backstageName, Namespace: ns}, update)
+	//	Expect(err).To(Not(HaveOccurred()))
+	//	update.Spec.Database = &api.Database{}
+	//	update.Spec.Database.EnableLocalDb = ptr.To(false)
+	//	err = k8sClient.Update(ctx, update)
+	//	Expect(err).To(Not(HaveOccurred()))
+	//	_, err = NewTestBackstageReconciler(ns).ReconcileAny(ctx, reconcile.Request{
+	//		NamespacedName: types.NamespacedName{Name: backstageName, Namespace: ns},
+	//	})
+	//	Expect(err).To(Not(HaveOccurred()))
+	//
+	//	Eventually(func(g Gomega) {
+	//		By("deleting Local Db StatefulSet, Service and Secret")
+	//		err = k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-%s", backstageName)}, &appsv1.StatefulSet{})
+	//		g.Expect(err).To(HaveOccurred())
+	//		g.Expect(errors.IsNotFound(err))
+	//
+	//		err = k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-%s", backstageName)}, &corev1.Service{})
+	//		g.Expect(err).To(HaveOccurred())
+	//		g.Expect(errors.IsNotFound(err))
+	//
+	//		err = k8sClient.Get(ctx, types.NamespacedName{Namespace: ns, Name: fmt.Sprintf("backstage-psql-secret-%s", backstageName)}, &corev1.Secret{})
+	//		g.Expect(err).To(HaveOccurred())
+	//		g.Expect(errors.IsNotFound(err))
+	//	}, time.Minute, time.Second).Should(Succeed())
+	//
+	//})
 
 	It("creates Backstage with disabled local DB and secret", func() {
 		backstageName := createAndReconcileBackstage(ctx, ns, api.BackstageSpec{
