44e36f12c3cea90c6e8344c019ddd96d753610d4
Fortune-Ndlovu <fndlovu@redhat.com>
2026-07-06 10:24:21 +0100

    Remove OpenShift-only restriction from OLM v1 usage text
    OLM v1 detection is now platform-agnostic via CRD presence,
    so the usage description should not restrict it to OpenShift.
    
    Signed-off-by: Fortune Ndlovu <fndlovu@redhat.com>

--- .rhdh/scripts/install-rhdh-catalog-source.sh
+++ .rhdh/scripts/install-rhdh-catalog-source.sh
@@ -55,8 +55,8 @@ function errorf() {
 function usage() {
   echo "
 This script streamlines testing IIB images by configuring an OpenShift or Kubernetes cluster to enable it to use the specified IIB image
-as a catalog source. On OLM v0, a CatalogSource is created in 'openshift-marketplace' (OpenShift) or 'olm' (Kubernetes). On OLM v1
-(OpenShift only), a ClusterCatalog and ClusterExtension are created instead. By default, the OLM version is auto-detected based on the
+as a catalog source. On OLM v0, a CatalogSource is created in 'openshift-marketplace' (OpenShift) or 'olm' (Kubernetes). On OLM v1,
+a ClusterCatalog and ClusterExtension are created instead. By default, the OLM version is auto-detected based on the
 presence of the ClusterExtension CRD. The catalog/resource name is 'operatorName-channelName', eg., rhdh-fast
 
 If IIB installation fails, see https://docs.engineering.redhat.com/display/CFC/Test and
