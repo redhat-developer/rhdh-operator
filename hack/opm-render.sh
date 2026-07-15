f93731007ebf98438dce938992c6418a762f07f2
Fortune-Ndlovu <fndlovu@redhat.com>
2026-07-15 15:38:42 +0100

    fix: use [[ instead of [ for conditional test in opm-render.sh
    Signed-off-by: Fortune Ndlovu <fndlovu@redhat.com>
    Signed-off-by: Fortune-Ndlovu <fndlovu@redhat.com>

--- hack/opm-render.sh
+++ hack/opm-render.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 : "${BUNDLE_IMGS:?BUNDLE_IMGS environment variable not set}"
 : "${OUTPUT_FILE:?OUTPUT_FILE environment variable not set}"
 
-if [ -f /etc/containers/registries.conf ] && grep -q '^\[registries' /etc/containers/registries.conf 2>/dev/null; then
+if [[ -f /etc/containers/registries.conf ]] && grep -q '^\[registries' /etc/containers/registries.conf 2>/dev/null; then
 	printf 'unqualified-search-registries = ["docker.io"]\n' > /tmp/opm-registries.conf
 	CONTAINERS_REGISTRIES_CONF=/tmp/opm-registries.conf "${OPM}" render "${BUNDLE_IMGS}" --output yaml >> "${OUTPUT_FILE}"
 else
