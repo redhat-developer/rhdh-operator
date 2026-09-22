4c1bfe0dc6fb639985764cc911aeaa73724c3b98
gazarenkov <gazarenkov@gmail.com>
2026-09-22 09:23:53 +0300

    mirrors

--- pkg/model/deployment.go
+++ pkg/model/deployment.go
@@ -125,8 +125,6 @@ func (b *BackstageDeployment) addToModel(model *BackstageModel, backstage api.Ba
 			b.podSpec().InitContainers[i].Command = []string{}
 			b.container().Args = []string{}
 
-			// TODO temporarily until stabilize
-			b.podSpec().InitContainers[i].ImagePullPolicy = corev1.PullAlways
 		}
 
 		if err := b.setDeployment(backstage); err != nil {
